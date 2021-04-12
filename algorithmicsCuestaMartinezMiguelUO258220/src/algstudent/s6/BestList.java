507295922e288d14536e8d508313338ffd0241ec
miguelcuestam <UO258220@uniovi.es>
2021-04-12 15:36:54 +0200

    Initial commit after reinstalling eclipse

---
+++ algorithmicsCuestaMartinezMiguelUO258220/src/algstudent/s6/BestList.java
@@ -0,0 +1,12 @@
+package algstudent.s6;
+
+public class BestList {
+	
+	public void main(String filename, int n) {
+		// Prints module of n songs from filename
+		// There are always 2 blocks
+		// For each song it should be checked in what block
+		// it should be (if any)
+	}
+
+}
